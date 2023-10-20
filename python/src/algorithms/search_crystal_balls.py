67a5a9dc89ad8f9c9af85b03ed1fe188d1924ca5
Toncho Zhelev <toncho.zhelev@gmail.com>
2023-10-20 23:11:57 +0300

    crystal balls

--- python/src/algorithms/search_crystal_balls.py
+++ python/src/algorithms/search_crystal_balls.py
@@ -23,3 +23,24 @@ def two_cristal_balls(breaks: list[bool]) -> int:
         i += 1
 
     return -1
+
+
+def two_cristal_balls2(breaks: list[bool]) -> int:
+    lenBreaks = len(breaks)
+    jmpAmount = floor(sqrt(lenBreaks))
+
+    for i in range(jmpAmount, lenBreaks, jmpAmount):
+        if breaks[i]:
+            break
+
+    i -= jmpAmount
+
+    j = 0
+
+    while j < jmpAmount and i < lenBreaks:
+        if breaks[i]:
+            return i
+        j += 1
+        i += 1
+
+    return -1
--- python/tests/test_crystal_balls.py
+++ python/tests/test_crystal_balls.py
@@ -1,6 +1,7 @@
 import random
 
-from algorithms.search_crystal_balls import two_cristal_balls
+from algorithms.search_crystal_balls import (two_cristal_balls,
+                                             two_cristal_balls2)
 
 
 def test_crystal_balls():
@@ -11,3 +12,13 @@ def test_crystal_balls():
 
     assert two_cristal_balls(data) == idx
     assert two_cristal_balls([False for _ in range(821)]) == -1
+
+
+def test_crystal_balls2():
+    random.seed()
+    idx = random.randrange(10000)
+
+    data = [x >= idx for x in range(10000)]
+
+    assert two_cristal_balls2(data) == idx
+    assert two_cristal_balls2([False for _ in range(821)]) == -1
